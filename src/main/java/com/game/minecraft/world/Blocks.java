fe0da1724ec91341f26445d91002608510c19b30
Lymeng Naret <lymengnaret@yahoo.com>
2025-02-04 07:13:02 -0800

    Add more blocks

--- src/main/java/com/game/minecraft/world/Blocks.java
+++ src/main/java/com/game/minecraft/world/Blocks.java
@@ -2,22 +2,55 @@ package com.game.minecraft.world;
 
 /** Enum representing unique blocks and their associated position within the atlas. */
 public enum Blocks {
-  GRASS(224, 16, 32, 0, 48, 0), // Top: grass, Bottom: dirt, Sides: grass block
-  DIRT(32, 0, 32, 0, 32, 0),
-  STONE(16, 0, 16, 0, 16, 0),
-  BEDROCK(16, 16, 16, 16, 16, 16);
+  // (y, x) = (row / up-down, column / left-right) in the atlas
+  // (0,2) + (0,3) + (1,14) Bottom: dirt, Sides: grass block, Top: grass
+  GRASS(14, 1, 2, 0, 3, 0),
+  // (0,2)
+  DIRT(2, 0, 2, 0, 2, 0),
+  // (4,8)
+  CLAY(8, 4, 8, 4, 8, 4),
+  // (1,3)
+  GRAVEL(3, 1, 3, 1, 3, 1),
+  // (1,2)
+  SAND(2, 1, 2, 1, 2, 1),
+  // (0,1)
+  STONE(1, 0, 1, 0, 1, 0),
+  // (1,1)
+  BEDROCK(1, 1, 1, 1, 1, 1),
+  // (2,5)
+  OBSIDIAN(5, 2, 5, 2, 5, 2),
+  // (1,0)
+  COBBLESTONE(0, 1, 0, 1, 0, 1),
+  // (2,4)
+  MOSS_COBBLESTONE(4, 2, 4, 2, 4, 2),
+  // (2,0)
+  GOLD_ORE(0, 2, 0, 2, 0, 2),
+  // (2,1)
+  IRON_ORE(1, 2, 1, 2, 1, 2),
+  // (2,2)
+  COAL_ORE(2, 2, 2, 2, 2, 2),
+  // (3,2)
+  DIAMOND_ORE(2, 3, 2, 3, 2, 3),
+  // (10, 0)
+  LAPIS_ORE(0, 10, 0, 10, 0, 10),
+  // (3,3)
+  REDSTONE_ORE(3, 3, 3, 3, 3, 3),
+  // (1,4) + (1,5)
+  OAKWOOD(5, 1, 5, 1, 4, 1),
+  ;
   private final byte topX, topY;
   private final byte bottomX, bottomY;
   private final byte sideX, sideY;
   private boolean isSolid;
+  private static final int PIXEL_OFFSET = 16;
 
   Blocks(int topX, int topY, int bottomX, int bottomY, int sideX, int sideY) {
-    this.topX = (byte) topX;
-    this.topY = (byte) topY;
-    this.bottomX = (byte) bottomX;
-    this.bottomY = (byte) bottomY;
-    this.sideX = (byte) sideX;
-    this.sideY = (byte) sideY;
+    this.topX = (byte) (topX * PIXEL_OFFSET);
+    this.topY = (byte) (topY * PIXEL_OFFSET);
+    this.bottomX = (byte) (bottomX * PIXEL_OFFSET);
+    this.bottomY = (byte) (bottomY * PIXEL_OFFSET);
+    this.sideX = (byte) (sideX * PIXEL_OFFSET);
+    this.sideY = (byte) (sideY * PIXEL_OFFSET);
     this.isSolid = true;
   }
 
